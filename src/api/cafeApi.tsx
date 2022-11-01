58b9ed3ce46f4ee1ee59b66b70489bcb90030cb5
thedraxx <thedraxxgames@gmail.com>
2022-11-01 13:24:13 -0300

    init

---
+++ src/api/cafeApi.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const baseURL = 'http://192.168.1.2:8080/api';
+
+const cafeApi = axios.create({baseURL});
+
+cafeApi.interceptors.request.use(async config => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    config.headers['x-token'] = token;
+  }
+  return config;
+});
+
+export default cafeApi;
